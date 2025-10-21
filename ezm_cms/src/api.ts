76b265dfbd9cefa5354334d1315318dccb3638ae
TanPN2001 <ronaltanface@gmail.com>
2025-10-21 22:15:09 +0700

    update

--- ezm_cms/src/api.ts
+++ ezm_cms/src/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "https://103.38.236.222:2402/api/v1" })
+const api = axios.create({ baseURL: "https://ezmansneaker.com/api/v1" })
 
 export default api
